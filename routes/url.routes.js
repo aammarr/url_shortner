792b3f4e5ed1512ffe30de446b2956a34e1ff99f
Ammar Ahmed <aammarr.ahmed@gmail.com>
2022-06-17 15:36:57 +0500

    add new url routes

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -5,5 +5,7 @@ const UrlController = require('../controllers/url.controller');
 
 router.get('/urls',UrlController.apiGetAllUrls);
 router.post('/url',UrlController.apiCreateNewShortUrl);
+router.get('/url/:id',UrlController.apiGetUrlById);
+router.delete('/url/:id',UrlController.apiDeleteUrlById);
 
-module.exports = router;
+module.exports = router;    
